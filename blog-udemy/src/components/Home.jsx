480f60fb99bf89b52eef55724d9a8b8dc07efa0d
ryusei-engineer <ryusei.engineer@gmail.com>
2024-08-06 22:47:08 +0900

    Home css

--- blog-udemy/src/components/Home.jsx
+++ blog-udemy/src/components/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import "./Home.css";
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {postList.map((post) => {
+        return(
+          <div className="postContents" key={post.id}>
+        <div className="postHeader">
+          <h1>{post.title}</h1>
+        </div>
+      <div className="postTextContainer">
+      {post.postText}
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>s</h3>
+        <button>削除</button>
+      </div>
+      </div>
+        )
+      })}
+    </div>
   )
 }
 
