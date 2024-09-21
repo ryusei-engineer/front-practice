1f23be27ac36ba53018cf3ad2ac7e5f0abdc566d
ryusei-engineer <ryusei.engineer@gmail.com>
2024-09-21 15:08:20 +0900

    twitter-clone sidebar-component 完成

---
+++ twitter-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import Timeline from './components/timeline/Timeline';
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      <Timeline />
+    </div>
+  );
+}
+
+export default App;
