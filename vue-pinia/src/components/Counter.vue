78cde0e96a5a12851ff78db52351cfa01ec8ede0
ryusei-engineer <ryusei.engineer@gmail.com>
2024-12-08 17:09:18 +0900

    vue-pinia 完成

---
+++ vue-pinia/src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore();
+</script>
+<template>
+  <div>
+    <h1>Count: {{ counterStore.count }}</h1>
+    <h2>Double Count: {{ counterStore.doubleCount }}</h2>
+    <button @click="counterStore.increment">Increment</button>
+    <button @click="counterStore.decreent">Decreent</button>
+  </div>
+</template>
