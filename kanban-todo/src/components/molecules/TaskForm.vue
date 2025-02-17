252ca00586ce91b8193e3230fb4e867fbd5e05be
ryusei-engineer <ryusei.engineer@gmail.com>
2025-02-18 01:25:05 +0900

    WIP: 一時コミット

--- kanban-todo/src/components/molecules/TaskForm.vue
+++ kanban-todo/src/components/molecules/TaskForm.vue
@@ -1,38 +1,36 @@
 <script setup>
-import { ref } from 'vue';
-import InputField from '../atoms/InputField.vue';
-import Select from '../atoms/SelectAtom.vue';
-import ButtonAtom from '../atoms/ButtonAtom.vue';
+import { ref } from 'vue'
+import InputField from '../atoms/InputField.vue'
+import Select from '../atoms/SelectAtom.vue'
+import ButtonAtom from '../atoms/ButtonAtom.vue'
 
-const task = ref("");
-const priority = ref("");
-const emergency = ref("");
+const task = ref('')
+const priority = ref('')
+const emergency = ref('')
 
 const priorityOptions = [
-{ value: "", label: "重要度" },
-  { value: "high", label: "高" },
-  { value: "medium", label: "中" },
-  { value: "low", label: "低" },
-];
+  { value: '', label: '重要度' },
+  { value: 'high', label: '高' },
+  { value: 'medium', label: '中' },
+  { value: 'low', label: '低' },
+]
 
 const emergencyOptions = [
-{ value: "", label: "緊急度" },
-  { value: "high", label: "高" },
-  { value: "medium", label: "中" },
-  { value: "low", label: "低" },
-];
+  { value: '', label: '緊急度' },
+  { value: 'high', label: '高' },
+  { value: 'medium', label: '中' },
+  { value: 'low', label: '低' },
+]
 
-const addTask = () => {
-}
+const addTask = () => {}
 </script>
 
 <template>
-  <InputField v-model="task" placeholder="タスクを入力"/>
-  <Select v-model="priority" :options="priorityOptions"/>
-  <Select v-model="emergency" :options="emergencyOptions"/>
+  <InputField v-model="task" placeholder="タスクを入力" />
+  <Select v-model="priority" :options="priorityOptions" />
+  <Select v-model="emergency" :options="emergencyOptions" />
   <ButtonAtom label="追加" @click="addTask" />
   {{ task }}
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
