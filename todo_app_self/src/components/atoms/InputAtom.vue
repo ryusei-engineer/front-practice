e091cdd4b6dc8123f7e459ba9839c7d28cb882e8
ryusei-engineer <ryusei.engineer@gmail.com>
2025-04-01 15:34:09 +0900

    todo_app_self InputAtom

---
+++ todo_app_self/src/components/atoms/InputAtom.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+defineProps({
+  modelValue: String
+});
+
+const emit = defineEmits(['update:modelValue'])
+
+const inputValue = (e) => {
+  emit('update:modelValue', e.target.value);
+}
+</script>
+<template>
+  <input :value="modelValue" @input="inputValue">
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ todo_app_self/src/components/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import ButtonAtom from '../atoms/InputAtom.vue';
+
+const hoge = ref("");
+</script>
+<template>
+<ButtonAtom v-model="hoge"></ButtonAtom>
+{{ hoge }}
+</template>
+
+<style scoped>
+
+</style>
